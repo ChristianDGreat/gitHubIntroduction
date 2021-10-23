9b0371c369fca799a79b2df05064cf859743a8a7
Christiandgreat09 <christianleonardoreyes@gmail.com>
2021-10-23 14:19:55 -0700

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
-print("Hello, Christian The Great!")
+persons = [{"christian":"king2"},{"christian":"king3"},{"christian":"king1"}]
+
+
+
+
+persons.sort(key=lambda k:k["christian"])
+
+print(persons)
